a8c4bd15923f99705e0071ebac3bba86744c5f4c
emamhasan1137 <emamhasan1137@gmail.com>
2021-07-13 12:27:32 +0600

    add pool

--- database/mongo_connection.js
+++ database/mongo_connection.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 var db,db_client;
 
 function connectToServer( callback ) {
-    MongoClient.connect(MONGO_DB_URI,  { useUnifiedTopology: true , useNewUrlParser: true,maxPoolSize:10 }, function( err, client ) {
+    MongoClient.connect(MONGO_DB_URI,  { useUnifiedTopology: true , useNewUrlParser: true,poolSize:10 }, function( err, client ) {
         db  = client.db(DB_OPTION[1].database);
         db_client=client;
         return callback( err );
