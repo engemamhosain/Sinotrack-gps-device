311bdda98ecbcce03ef27263a1f7c42956c4b134
emamhasan1137 <emamhasan1137@gmail.com>
2021-07-13 12:18:40 +0600

    add pool

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -36,7 +36,7 @@ module.exports = {
        "repo" : `https://gitlab+deploy-token-509262:JGhetBnT3hsGdesxY6Ea@gitlab.com/emamhasan1137/GpsDeviceLocation.git`,
        "path" : "/data4tb/other/Sintorack/TestDeploy",
        "pre-setup" : "mkdir TestDeploy",
-       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+       'post-deploy' : 'npm install && pm2 delete sinotrackServer && pm2 start ecosystem.config.js --env production'
     }
 
 
