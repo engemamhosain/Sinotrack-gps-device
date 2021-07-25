688a5b9dca38637545205f4a7af93363e4e2335b
emamhasan1137 <emamhasan1137@gmail.com>
2021-07-25 11:32:48 +0600

    deploy 1

--- database/mongo_connection.js
+++ database/mongo_connection.js
@@ -14,13 +14,13 @@ class MongoDbClient{
       insertMongoData(obj);
     }
   
-    
+
     insertMongoData =(obj)=> {
 
         MongoClient.connect(MONGO_DB_URI,{useUnifiedTopology: true},function(err, db) {
             assert.equal(null, err);
 
-            const db  = client.db(DB_OPTION[1].database);
+       //     const database  = client.db(DB_OPTION[1].database);
             const collection_gps_device_location = db.collection(collection_name+collectionName);
             collection_gps_device_location.insertOne(this.obj,function(err, item){
                 db.close();
