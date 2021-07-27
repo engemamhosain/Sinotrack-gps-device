2214cc3206aa877d2b55019254d1352dbfd78c8d
emamhasan1137 <emamhasan1137@gmail.com>
2021-07-27 14:59:15 +0600

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,7 +29,6 @@ module.exports = {
        "ref"  : "origin/master",
        "repo" : `https://gitlab+deploy-token-509262:JGhetBnT3hsGdesxY6Ea@gitlab.com/emamhasan1137/GpsDeviceLocation.git`,
        "path" : " /Users/hello/Gps/TestDeploy",
-       "pre-setup" : "",
        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env local'
     },
 
