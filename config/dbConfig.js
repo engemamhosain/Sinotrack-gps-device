7f0121d5f2482cae0e4fdffa3bae96f73369a913
emamhasan1137 <emamhasan1137@gmail.com>
2021-09-07 11:06:03 +0600

    env credentials update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,13 +4,13 @@ const DB_OPTION=[{
     host: process.env.MYSQL_HOST,  
     user:  process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE
 },{
-    port:MONGO_PORT,
-    host:MONGO_HOST,
-    user:MONGO_USER,
-    password:MONGO_PASSOWRD,
-    database:MONGO_DATABASE
+    port:process.env.MONGO_PORT,
+    host:process.env.MONGO_HOST,
+    user:process.env.MONGO_USER,
+    password:process.env.MONGO_PASSOWRD,
+    database:process.env.MONGO_DATABASE
 
 }
 ]
