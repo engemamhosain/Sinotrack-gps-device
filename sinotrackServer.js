305c36b980eb9e07b8fc271589577b2885904eae
emamhasan1137 <emamhasan1137@gmail.com>
2021-09-13 10:29:08 +0600

    limit add

--- sinotrackServer.js
+++ sinotrackServer.js
@@ -35,7 +35,7 @@ const {DB_OPTION} = require("./config/dbConfig")
 
 
 const CONNECTION = mysql.createPool({
-  connectionLimit: 250,
+  connectionLimit: 3000,
   host:DB_OPTION[0].host,
   user: DB_OPTION[0].user,
   password:DB_OPTION[0].password,
