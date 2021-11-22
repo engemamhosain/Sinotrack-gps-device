ed735e52eeec57408ea240ad8b6418886f9cf32d
emamhasan1137 <emamhasan1137@gmail.com>
2021-11-22 15:43:33 +0600

    bug fix

--- database/mongo_connection.js
+++ database/mongo_connection.js
@@ -8,10 +8,9 @@ const collection_name=["gps_device_location_"];
 class MongoDbClient{
     obj=null;
     
-    constructor(obj,mongodb) {
+    constructor(obj) {
     this.obj=obj;
       this.insertMongoData(obj);
-      
     }
   
 
@@ -41,47 +40,17 @@ class MongoDbClient{
 
                 if(err){
                     throw err
-               }
+               }else{
 
+                if(obj.imei_id ==1170948091){
+                  console.log(item);
+                }
+          
+               }
             });
 
         });
     }    
-
-
-
-
-    insertMongoDataPool =(obj)=> {
-      
-
-      MongoClient.connect(MONGO_DB_URI,{useUnifiedTopology: true},function(err, client) {
-        if(err){
-              throw err
-        }
-        
-        let date=new Date();
-        let month=date.getMonth()+1;
-        let collectionName=date.getDate()+"_"+month+"_"+date.getFullYear();
-         const database  = client.db(DB_OPTION[1].database);
-          const collection_gps_device_location = database.collection(collection_name+collectionName);
-        
-          collection_gps_device_location.insertOne(obj,function(err, item){
-
-
-        
-
-            //  collection_gps_device_location.createIndex({imei_id:1})
-              client.close();
-
-              if(err){
-                  throw err
-             }
-
-          });
-
-      });
-  }  
-
 }
 
 module.exports = MongoDbClient
